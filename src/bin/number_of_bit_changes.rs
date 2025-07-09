1b55ea1750e4483f7f9703062f1bcfc85cf81bf6
MarioMottl <mario.mottl@pm.me>
2025-07-09 16:45:08 +0200

    Number of Bit Changes to Make Two Integers Equal

---
+++ src/bin/number_of_bit_changes.rs
@@ -0,0 +1,14 @@
+pub fn min_changes(n: i32, k: i32) -> i32 {
+    if k & !n != 0 {
+        return -1;
+    }
+
+    (n & !k).count_ones() as i32
+}
+
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
