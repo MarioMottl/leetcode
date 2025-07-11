db0c7bd71e6b818226d9607e8975fcc06cbe54e7
MarioMottl <mario.mottl@pm.me>
2025-07-11 19:57:01 +0200

    easy(binary-search): first bad version ( update to make it buildable )

--- src/bin/first_bad_version.rs
+++ src/bin/first_bad_version.rs
@@ -1,9 +1,13 @@
-pub fn first_bad_version(&self, n: i32) -> i32 {
+fn is_bad_version(version: i32) -> bool {
+    return false;
+}
+
+pub fn first_bad_version(n: i32) -> i32 {
     let (mut left, mut right) = (0, n);
 
     while left < right {
         let mid = left + (right - left) / 2;
-        if self.isBadVersion(mid) {
+        if is_bad_version(mid) {
             right = mid;
         } else {
             left = mid + 1;
