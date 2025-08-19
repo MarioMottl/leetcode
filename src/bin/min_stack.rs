839b2e7f183923551095c5a1c55a2c743b8aa8b4
MarioMottl <mario.mottl@pm.me>
2025-08-19 23:47:05 +0200

    medium(stack): min stack

---
+++ src/bin/min_stack.rs
@@ -0,0 +1,46 @@
+struct MinStack {
+    stack: Vec<i32>,
+    mins: Vec<i32>,
+}
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl MinStack {
+    fn new() -> Self {
+        Self {
+            stack: Vec::new(),
+            mins: Vec::new(),
+        }
+    }
+
+    fn push(&mut self, val: i32) {
+        self.stack.push(val);
+        if let Some(&m) = self.mins.last() {
+            self.mins.push(m.min(val));
+        } else {
+            self.mins.push(val);
+        }
+    }
+
+    fn pop(&mut self) {
+        self.stack.pop();
+        self.mins.pop();
+    }
+
+    fn top(&self) -> i32 {
+        *self.stack.last().unwrap()
+    }
+
+    fn get_min(&self) -> i32 {
+        *self.mins.last().unwrap()
+    }
+}
+
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
